cc4ee3b3e32c4b26331192cdb06adf7663cfba90
woodspork <wooden_spork@hotmail.com>
2023-01-26 17:41:10 -0800

    come code cleanup

--- main.go
+++ main.go
@@ -1,33 +1,55 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math/rand"
+)
 
-func cardsAreEqual(card1 []int, card2 []int) bool {
-  counter := 0
-  for i:=0; i<len(card1); i++ {
-    if (card1[i] == card2[i]){
-      counter++
-    }
-  }
+func pickRandomCard(setsBoard [][4]int) [4]int {
+  r := rand.Intn(81)
+  fmt.Println("random number", r)
 
-  if (counter == 4){
-    return true
-  }
-
-  return false
+  return setsBoard[r]
 }
 
-func removeCard(setsBoard [][]int, card []int) [][]int {
-  var cardsAreEqualVal bool
-  for i:=0; i<12; i++ {
-    cardsAreEqualVal = cardsAreEqual(setsBoard[i], card)
-    if (cardsAreEqualVal == true) {
-      setsBoard[i] = setsBoard[len(setsBoard)-1]
-      return setsBoard[:len(setsBoard)-1]
+func cardsAreEqual(card1 [4]int, card2 [4]int) bool {
+  return card1 == card2
+}
+
+func addCard(setsBoard [][4]int, card [4]int) [][4]int {
+  return append(setsBoard, card)
+}
+
+func generateSetsBoard() [][4]int {
+  setsBoard, helperBoard := [][4]int{{0,0,0,0}}, [][4]int{}
+
+  for j:=0; j<4; j++ {
+    for i:=0; i<len(setsBoard); i++ {
+      card1 := setsBoard[i]
+      card1[j] = 1
+      helperBoard = addCard(helperBoard, card1)
+
+      card2 := setsBoard[i]
+      card2[j] = -1
+      helperBoard = addCard(helperBoard, card2)
     }
+
+    setsBoard = append(setsBoard, helperBoard...)
+    helperBoard = [][4]int{}
   }
 
   return setsBoard
+}
+
+func removeCard(setsBoard [][4]int, card [4]int) (newSetsBoard [][4]int, removedCard [4]int) {
+  for i:=0; i<12; i++ {
+    if (setsBoard[i] == card){
+      setsBoard[i] = setsBoard[len(setsBoard) - 1]
+      return setsBoard[:len(setsBoard) - 1], card
+    }
+  }
+
+  return setsBoard, card
 }
 
 func isSet(board1 []int, board2 []int, board3 []int) bool {
@@ -63,6 +85,15 @@ func identifySet(setsBoard [][]int){
 }
 
 func main() {
-  setsBoard := [][]int{{0,1,0,0}, {0,1,0,0}, {0,1,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}}
-  identifySet(setsBoard)
+
+  board1 := generateSetsBoard()
+  fmt.Println(board1)
+  fmt.Println(len(board1))
+
+  newBoard, cardRemoved := removeCard(board1, [4]int{0,0,0,0})
+  fmt.Println("newBoard", newBoard)
+  fmt.Println("cardRemoved", cardRemoved)
+
+  randomCard := pickRandomCard(newBoard)
+  fmt.Println("random card", randomCard)
 }
