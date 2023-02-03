7574114afcd6b8d8c5604334df4c8bcdc21548bc
woodspork <wooden_spork@hotmail.com>
2023-02-03 00:02:42 -0800

    some cleanup

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
   "math/rand"
 )
 
-func pickRandomCard(setsBoard [][4]int) [4]int {
+func pickRandomCard(setsBoard [][4]int) ([4]int, int) {
   r := rand.Intn(81)
   fmt.Println("random number", r)
 
-  return setsBoard[r]
+  return setsBoard[r], r
 }
 
 func cardsAreEqual(card1 [4]int, card2 [4]int) bool {
@@ -20,7 +20,7 @@ func addCard(setsBoard [][4]int, card [4]int) [][4]int {
   return append(setsBoard, card)
 }
 
-func generateSetsBoard() [][4]int {
+func generateAllCards() [][4]int {
   setsBoard, helperBoard := [][4]int{{0,0,0,0}}, [][4]int{}
 
   for j:=0; j<4; j++ {
@@ -41,15 +41,22 @@ func generateSetsBoard() [][4]int {
   return setsBoard
 }
 
-func removeCard(setsBoard [][4]int, card [4]int) (newSetsBoard [][4]int, removedCard [4]int) {
-  for i:=0; i<12; i++ {
-    if (setsBoard[i] == card){
-      setsBoard[i] = setsBoard[len(setsBoard) - 1]
-      return setsBoard[:len(setsBoard) - 1], card
-    }
+func generateSetsBoard(unpickedCards [][4]int, setsBoard [][4]int) ([][4]int, []int) {
+  numberOfCards := len(setsBoard)
+  indicesOfCardsPicked := []int{}
+  for i:=0; i<12-numberOfCards; i++ {
+    card, index := pickRandomCard(unpickedCards)
+    indicesOfCardsPicked = append(indicesOfCardsPicked, index)
+    setsBoard = addCard(setsBoard, card)
   }
 
-  return setsBoard, card
+  return setsBoard, indicesOfCardsPicked
+}
+
+func removeCard(setsBoard [][4]int, i int) (newSetsBoard [][4]int, removedCard [4]int) {
+  card := setsBoard[i]
+  setsBoard[i] = setsBoard[len(setsBoard) - 1]
+  return setsBoard[:len(setsBoard) - 1], card
 }
 
 func isSet(board1 []int, board2 []int, board3 []int) bool {
@@ -68,7 +75,7 @@ func isSet(board1 []int, board2 []int, board3 []int) bool {
   return false
 }
 
-func identifySet(setsBoard [][]int){
+func identifySet(setsBoard [][]int) (int, int, int) {
   var identifySet bool;
   for i:=0; i<len(setsBoard); i++ {
     for j:=0; j<len(setsBoard); j++ {
@@ -76,24 +83,54 @@ func identifySet(setsBoard [][]int){
         if (i != j && j != k && i != k){
           identifySet = isSet(setsBoard[i], setsBoard[j], setsBoard[k])
           if (identifySet == true){
-            fmt.Println(setsBoard[i], setsBoard[j], setsBoard[k])
+            return i, j, k
           }
         }
       }
     }
   }
+  return 0, 0, 0
+}
+
+func allColorsExist(setsBoard [][]int) bool {
+  colorsMap := make(map[string]bool)
+  for i:=0; i<len(setsBoard); i++ {
+    if (setsBoard[i][0] == 1){
+      colorsMap["blue"] = true
+    }
+    if (setsBoard[i][0] == 1){
+      colorsMap["red"] = true
+    }
+    if (setsBoard[i][0] == 1){
+      colorsMap["green"] = true
+    }
+  }
+
+  return (colorsMap["blue"] && colorsMap["red"] && colorsMap["green"])
+}
+
+func allNumbersExist(setsBoard [][]int) bool {
+  numbersMap := make(map[int]bool)
+  for i:=0; i<len(setsBoard); i++ {
+    if (setsBoard[i][1] == 1){
+      numbersMap[1] = true
+    }
+
+    if (setsBoard[i][1] == 0){
+      numbersMap[1] = true
+    }
+
+    if (setsBoard[i][1] == 1){
+      numbersMap[1] = true
+    }
+  }
 }
 
 func main() {
+  allCards := generateAllCards()
+  fmt.Println(allCards)
+  setsBoard, indicesOfCardsPicked := generateSetsBoard(allCards, [][4]int{})
+  fmt.Println("setsBoard", setsBoard)
+  fmt.Println("indicesOfCardsPicked", indicesOfCardsPicked)
 
-  board1 := generateSetsBoard()
-  fmt.Println(board1)
-  fmt.Println(len(board1))
-
-  newBoard, cardRemoved := removeCard(board1, [4]int{0,0,0,0})
-  fmt.Println("newBoard", newBoard)
-  fmt.Println("cardRemoved", cardRemoved)
-
-  randomCard := pickRandomCard(newBoard)
-  fmt.Println("random card", randomCard)
 }
